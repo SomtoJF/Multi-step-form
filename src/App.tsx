4ae4717c59c8d670f007235251be90c70df07087
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-08-24 02:11:56 +0100

    move form and sidebar into a popover

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.sass";
 import SideBar from "./components/SideBar";
 
 function App() {
+	const [currentStep, setCurrentStep] = useState(1);
 	return (
 		<div id="form-page">
-			<SideBar />
-			<form action="">this is a form</form>
+			<div id="popover">
+				<SideBar currentStep={currentStep} setCurrentStep={setCurrentStep} />
+				<form action="">this is a form</form>
+			</div>
 		</div>
 	);
 }
