a5d93e25c2624dd718c718ff30800ce427755bbb
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-08-25 20:16:22 +0100

    Initial commit

---
+++ src/components/Confirmation.tsx
@@ -0,0 +1,16 @@
+import confirmIcon from "../assets/icon-thank-you.svg";
+import "../styles/Confirmation.sass";
+
+export default function Confirmation() {
+	return (
+		<div id="confirmation-section">
+			<img src={confirmIcon} alt="thank you icon" />
+			<h1>Thank You</h1>
+			<p className="information-paragraph">
+				Thanks for confirming your subscription! We hope you have fun using our
+				platform. If you ever need support, please feel free to email us at
+				support@loremgaming.com.
+			</p>
+		</div>
+	);
+}
