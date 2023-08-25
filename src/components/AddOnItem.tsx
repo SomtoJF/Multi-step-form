9b36c45d4312e8b30ed9834e419eeb683b8c7fbf
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-08-25 13:57:49 +0100

    BugFix: previous add ons persist through change of subscription plan

--- src/components/AddOnItem.tsx
+++ src/components/AddOnItem.tsx
@@ -1,16 +1,43 @@
-import { addOnType } from "../App";
+import React, { MouseEvent, useEffect, useState } from "react";
+import { FormDataInterface, addOnType } from "../App";
+import checkMarkIcon from "../assets/icon-checkmark.svg";
 
 type propType = {
 	item: addOnType;
+	formData: FormDataInterface;
+	setFormData: (formData: FormDataInterface) => void;
 };
 
-export default function AddOnItem({ item }: propType) {
+export default function AddOnItem({ item, formData, setFormData }: propType) {
+	const [isSelected, setIsSelected] = useState(false);
+
+	useEffect(() => {
+		formData.addOns.includes(item) ? setIsSelected(true) : setIsSelected(false);
+	}, [formData.addOns]);
+
+	function handleAddOnSelection(e: MouseEvent) {
+		isSelected ? removeThisItem() : addThisItem();
+	}
+	function addThisItem() {
+		setFormData({ ...formData, addOns: formData.addOns.concat(item) });
+	}
+	function removeThisItem() {
+		setFormData({
+			...formData,
+			addOns: formData.addOns.filter((addOn) => addOn != item),
+		});
+	}
 	return (
-		<button type="button" className="add-on-item">
-			<button type="button" className="checkbox"></button>
+		<div
+			className={isSelected ? "add-on-item selected" : "add-on-item"}
+			onClick={handleAddOnSelection}
+		>
+			<button type="button" className="checkbox">
+				{isSelected ? <img src={checkMarkIcon} alt="chekcmark-icon" /> : null}
+			</button>
 			<h3>{item.name}</h3>
 			<p>{item.description}</p>
 			<div>{item.price}</div>
-		</button>
+		</div>
 	);
 }
